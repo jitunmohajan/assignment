69b66ba09c4bb8a0346f151a292b65712f817efb
jitunmohajan <jitunmohajan@gmail.com>
2018-11-22 04:05:45 +1300

    new update

--- myapp/resources/views/pages/login.blade.php
+++ myapp/resources/views/pages/login.blade.php
@@ -10,10 +10,10 @@
     <meta name="author" content="">
 
     <title>My Project</title>
-<link href="{{asset('LoginBackend')}}/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
-    <link href="{{asset('LoginBackend')}}/vendor/metisMenu/metisMenu.min.css" rel="stylesheet">
-    <link href="{{asset('LoginBackend')}}/dist/css/sb-admin-2.css" rel="stylesheet">
-    <link href="{{asset('LoginBackend')}}/vendor/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+<link href="{{asset('LoginBackend/vendor/bootstrap/css/bootstrap.min.css')}}" rel="stylesheet">
+    <link href="{{asset('LoginBackend/vendor/metisMenu/metisMenu.min.css')}}" rel="stylesheet">
+    <link href="{{asset('LoginBackend/dist/css/sb-admin-2.css')}}" rel="stylesheet">
+    <link href="{{asset('LoginBackend/vendor/font-awesome/css/font-awesome.min.css')}}" rel="stylesheet" type="text/css">
 
 
 </head>
@@ -37,11 +37,7 @@
                                 <div class="form-group">
                                     <input class="form-control" placeholder="Password" name="password" type="password" value="">
                                 </div>
-                                <div class="checkbox">
-                                    <label>
-                                        <input name="remember" type="checkbox" value="Remember Me">Remember Me
-                                    </label>
-                                </div>
+                                
                                 <!-- Change this to a button or input when using this as a form -->
                                 <button type="submit" class="btn btn-lg btn-success btn-block">Login</button>
                             </fieldset>
@@ -55,17 +51,7 @@
         </div>
     </div>
 
-    <!-- jQuery -->
-    <script src="../vendor/jquery/jquery.min.js"></script>
-
-    <!-- Bootstrap Core JavaScript -->
-    <script src="../vendor/bootstrap/js/bootstrap.min.js"></script>
-
-    <!-- Metis Menu Plugin JavaScript -->
-    <script src="../vendor/metisMenu/metisMenu.min.js"></script>
-
-    <!-- Custom Theme JavaScript -->
-    <script src="../dist/js/sb-admin-2.js"></script>
+    
 
 </body>
 
